ce115f2a9077aec494440f5094cdba639d63ca18
Deivi <deivi54357326@gmail.com>
2025-03-03 23:15:28 +0100

    Implement smooth scrolling and update header styles; adjust layout and navigation links

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const fragment = this.router.url.split('#')[1];
+        if (fragment) {
+          this.scrollToSection(fragment);
+        }
+      }
+    });
+  }
+
+  scrollToSection(id: string) {
+    setTimeout(() => {
+      const section = document.getElementById(id);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }, 100);
+  }
+
 
 }
