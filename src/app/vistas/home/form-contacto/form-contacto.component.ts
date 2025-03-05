db37923dba72320e9ededb1aa3bee913c998fc4a
Deivi <deivi54357326@gmail.com>
2025-03-05 19:42:56 +0100

    Add footer and contact form components; update styles and layout for improved structure

---
+++ src/app/vistas/home/form-contacto/form-contacto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SectionTitleComponent } from "../../../shared/section-title/section-title.component";
+
+@Component({
+  selector: 'app-form-contacto',
+  imports: [SectionTitleComponent],
+  templateUrl: './form-contacto.component.html',
+  styleUrl: './form-contacto.component.css'
+})
+export class FormContactoComponent {
+
+}
--- src/app/vistas/home/home.component.ts
+++ src/app/vistas/home/home.component.ts
@@ -4,10 +4,12 @@ import { BanerComponent } from "./baner/baner.component";
 import { AboutComponent } from "./about/about.component";
 import { ServiciosComponent } from "./servicios/servicios.component";
 import { PersonalComponent } from './personal/personal.component';
+import { FormContactoComponent } from "./form-contacto/form-contacto.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, BanerComponent, AboutComponent, ServiciosComponent, PersonalComponent],
+  imports: [HeaderComponent, BanerComponent, AboutComponent, ServiciosComponent, PersonalComponent, FormContactoComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
